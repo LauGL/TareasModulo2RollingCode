6abb5be91a569f631a3c1ef33dec7b231e431dec
Laura GL <laugranadolopez@gmail.com>
2023-07-04 21:08:17 +0200

    Primer commit

---
+++ tareas.js
@@ -0,0 +1,22 @@
+// 1.- Escribe un programa de una sola línea que haga que aparezca en la pantalla un alert que diga “un mensaje”.
+
+    alert("Bienvenido a js");
+
+// 2.- Escribe un programa de una sola línea que escriba en la pantalla un texto que diga «Hello World» (document.write).
+
+document.write("Hello world")
+
+// 3.- Escribe un programa de una sola línea que escriba en la pantalla el resultado de sumar 3 + 5.
+
+document.write(`<br/>El resultado de 3 + 5 es igual a ${3+5}.` );
+
+// 4.- Escribe un programa de dos líneas que pida el nombre del usuario con un prompt y escriba un texto que diga «Hola nombreUsuario»
+// Ejemplo: 
+
+// input: Coder 
+// Output: Hola Coder
+
+let username = prompt("Por favor, introduzca su nombre de usuario: ")
+document.write(`<br/>Bienvenido, ${username}!`)
+
+
