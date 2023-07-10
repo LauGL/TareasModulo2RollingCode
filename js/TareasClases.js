3de8c0264d8581821cafc3a73e476fdffa0df055
Laura GL <laugranadolopez@gmail.com>
2023-07-10 15:16:28 +0200

    ⬆️ UPDATED: HW finished

--- js/TareasClases.js
+++ js/TareasClases.js
@@ -2,26 +2,26 @@
 
 //En un establecimiento de juegos existen cuatro salas: Arcade, Consolas, Rol, Realidad Virtual. Si un usuario paga 4 créditos puede acceder a todas, si paga 3 solo podrá acceder a las tres primeras, si paga 2 a las dos primeras y si paga 1 solo a la primera sala.
 
-let credito = parseInt(
-  prompt(
-    "Por favor introduzca créditos para acceder a las salas: \n4 créditos:Arcade, Consolas, Rol, Realidad Virtual \n3 créditos: Arcade, Consolas y Rol \n2 créditos: Arcade y Consolas \n1 crédito: Arcade"
-  )
-);
+// let credito = parseInt(
+//   prompt(
+//     "Por favor introduzca créditos para acceder a las salas: \n4 créditos:Arcade, Consolas, Rol, Realidad Virtual \n3 créditos: Arcade, Consolas y Rol \n2 créditos: Arcade y Consolas \n1 crédito: Arcade"
+//   )
+// );
 
-switch (true) {
-  case credito === 4:
-    document.write(
-      "Tiene acceso a todas las salas: Arcade, Consolas, Rol y Realidad virtual."
-    );
-    break;
-  case credito === 3:
-    document.write("Tiene acceso a tres salas: Arcade, Consolas y Rol.");
-    break;
-  case credito === 2:
-    document.write("Tiene acceso a dos salas: Arcade y Consolas.");
-    break;
-  case credito === 1:
-    document.write("Tiene acceso a la sala Arcade.");
-  default:
-    document.write("Introduce un crédito correcto.");
-}
+// switch (true) {
+//   case credito === 4:
+//     document.write(
+//       "Tiene acceso a todas las salas: Arcade, Consolas, Rol y Realidad virtual."
+//     );
+//     break;
+//   case credito === 3:
+//     document.write("Tiene acceso a tres salas: Arcade, Consolas y Rol.");
+//     break;
+//   case credito === 2:
+//     document.write("Tiene acceso a dos salas: Arcade y Consolas.");
+//     break;
+//   case credito === 1:
+//     document.write("Tiene acceso a la sala Arcade.");
+//   default:
+//     document.write("Introduce un crédito correcto.");
+// }
