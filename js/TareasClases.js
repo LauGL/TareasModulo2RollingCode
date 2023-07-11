10c409a8b6a2808cdb3844dbd99282ba085a3172
Laura GL <laugranadolopez@gmail.com>
2023-07-11 19:19:22 +0200

    ⬆️ ⬆️ UPDATED nuevo ejercicio de clase realizado

--- js/TareasClases.js
+++ js/TareasClases.js
@@ -25,3 +25,35 @@
 //   default:
 //     document.write("Introduce un crédito correcto.");
 // }
+
+// Tarea clase 10/7/23
+
+//Escribe un programa que pida una frase, devuelva la cantidad de vocales que tiene la frase y muestre en consola cuales son.
+
+let frase = prompt("Por favor, ingrese una frase:");
+fraseEnMinusculas = frase.toLowerCase();
+
+let vocales = ["a", "e", "i", "o", "u"];
+
+let contador = 0;
+
+let vocalesEncontradas = [];
+
+if (!isNaN(frase)) {
+  alert("Lo sentimos, el valor introducido no es válido.");
+}
+
+for (let i = 0; i < fraseEnMinusculas.length; i++) {
+  let letra = fraseEnMinusculas[i];
+
+  if (vocales.includes(letra)) {
+    contador++;
+
+    if (!vocalesEncontradas.includes(letra)) {
+      vocalesEncontradas.push(letra);
+    }
+  }
+}
+
+console.log("Laantidad de vocales en su frase es:", contador);
+console.log("Y las vocales encontradas han sido:", vocalesEncontradas);
