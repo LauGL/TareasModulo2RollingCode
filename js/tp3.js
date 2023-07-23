c4c0b52ed6d29c4d752e8a150f4a373d7f7c438f
Laura GL <laugranadolopez@gmail.com>
2023-07-23 21:27:49 +0200

    ⬆️ Updated: formating

--- js/tp3.js
+++ js/tp3.js
@@ -84,4 +84,18 @@
 //   }
 // };
 
-multiplicar();
+// multiplicar();
+
+// function tablaDeMultiplicar(
+//   numero = parseInt(prompt("Ingrese el valor de la tabla a calcular")),
+//   vueltas = parseInt(prompt("del 1 al cuanto?"))
+// ) {
+//   if (isNaN(numero) || isNaN(vueltas)) {
+//     return alert("Debe ingresar un número");
+//   }
+
+//   document.write(`Tabla del ${numero}<br>`);
+//   for (let index = 1; index <= vueltas; index++) {
+//     document.write(`${numero} x ${index} = ${numero * index}<br>`);
+//   }
+// }
